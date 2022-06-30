40c621230894617d1d0ed3861f5ede8a4a65e2d6
CharnyshSv <sv.charnysh@gmail.com>
2022-06-30 23:55:25 +0300

    add second way

--- 3_3.py
+++ 3_3.py
@@ -3,3 +3,9 @@ age: int = 10
 town: str = 'Minsk'
 text = f"My name is {name}. I'm {age} years old. I am from {town}."
 print(text)
+
+name: str = 'Alherd'
+age: int = 10
+town: str = 'Minsk'
+text = 'My name is {name}. I\'m {age} years old. I\'m from {town}.'.format(name=name, age=age, town=town)
+print(text)
