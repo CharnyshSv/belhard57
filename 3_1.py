7b5af2be380df863ab1a69ebc501e78c79c2319a
CharnyshSv <sv.charnysh@gmail.com>
2022-06-30 22:58:04 +0300

    second way

--- 3_1.py
+++ 3_1.py
@@ -2,5 +2,7 @@ word1: str = "Hello"
 word2: str = "Python"
 print(word1, word2, sep="-")
 
-text: str = "Hello my dear friend"
-print(text, sep="-")
+text: str = 'Hello my dear friend'
+words: str = text.split(' ')
+text: str = '-'.join(words)
+print(text)
