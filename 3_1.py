23fb83d227865c59c7222a85939b0c88b7b1d42e
CharnyshSv <108144044+CharnyshSv@users.noreply.github.com>
2022-07-01 21:10:13 +0300

    Update 3_1.py

--- 3_1.py
+++ 3_1.py
@@ -2,7 +2,7 @@ word1: str = "Hello"
 word2: str = "Python"
 print(word1, word2, sep="-")
 
-text: str = 'Hello my dear friend'
+text: str = input('Enter text') # Hello my dear friend
 words: str = text.split(' ')
 text: str = '-'.join(words)
 print(text)
