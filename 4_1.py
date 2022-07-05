ae013611d2cd11c9e0a2f2745fc717437c7b329e
CharnyshSv <sv.charnysh@gmail.com>
2022-07-05 22:21:31 +0300

    add resolve

--- 4_1.py
+++ 4_1.py
@@ -1,5 +1,5 @@
-#
-n: int = input('Enter number')  # любое число
+# Заполнить список степенями числа 2 (от 2^1 до 2^n)
+n: int = int(input('Enter number'))  # любое число
 range(int(n))
-numbers: [2 ** i for i in (n)]
+numbers: list[int] = [2 ** i for i in range(n)]
 print(numbers)
