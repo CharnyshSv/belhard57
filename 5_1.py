803624814de63c4f3280e3b08b9fe079f6c8631a
CharnyshSv <sv.charnysh@gmail.com>
2022-07-07 22:17:31 +0300

    add resolve

---
+++ 5_1.py
@@ -0,0 +1,7 @@
+#
+n: int = int(input("Задайте количество чисел:"))
+m: int = int(input("Задайте кратность числа:"))
+k: int = int(input("Задайте больше какого числа:"))
+for i in range(101):
+    if i % m and i > k
+        print(i)
