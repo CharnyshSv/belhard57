4bc4637ffa09bceb5f3c36ab63bf5a1771adc66d
CharnyshSv <sv.charnysh@gmail.com>
2022-07-07 23:22:28 +0300

    add resolve

--- 5_1.py
+++ 5_1.py
@@ -1,7 +1,17 @@
 #
-n: int = int(input("Задайте количество чисел:"))
-m: int = int(input("Задайте кратность числа:"))
-k: int = int(input("Задайте больше какого числа:"))
+#n: int = int(input("Enter number:"))
+m: int = int(input("Enter number:"))
+k: int = int(input("Enter number:"))
 for i in range(101):
-    if i % m and i > k
-        print(i)
+    if i % m == 0 and i > k:
+       print(i)
+        if i<=50:
+            continue
+        else:
+            print("It's last number")
+
+# while i % m == 0 and i > k:
+#    print(i)
+#    if counter != 6:
+#        continue
+#   break
---
+++ 5_2.py
@@ -0,0 +1,7 @@
+# Калькулятор
+a: int = int(input("Enter number 1:"))
+b: int = int(input("Enter number 2:"))
+oper: int = int(input("Select operation:"))
+
+if oper == +:
+    print(a+b)
