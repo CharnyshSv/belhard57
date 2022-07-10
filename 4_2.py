d014211054f1bbca5a9f7b3d83672f5d3274566c
CharnyshSv <108144044+CharnyshSv@users.noreply.github.com>
2022-07-10 19:07:46 +0300

    Revert "Homework"

--- 4_2.py
+++
@@ -1,6 +0,0 @@
-# Без использования collections, написать программу, которая будет
-# создавать словарь для подсчитывания количества вхождений каждой
-# буквы в текст введенный с клавиатуры
-text: str = input('Enter text')  # вести текст
-dictionary = {symbol: text.count(symbol) for symbol in text}
-print(dictionary)
