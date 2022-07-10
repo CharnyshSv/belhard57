5efbf0279c4571911c688606454a4ebab5acdc15
CharnyshSv <sv.charnysh@gmail.com>
2022-07-10 19:05:50 +0300

    add resolve 3

--- 5_1.py
+++ 5_1.py
@@ -1,17 +1,10 @@
-# Вывести первые чисел кратные M и больше K
-#n: int = int(input("Enter number:"))
-m: int = int(input("Enter number:"))
-k: int = int(input("Enter number:"))
-for i in range(101):
-    if i % m == 0 and i > k:
-       print(i)
-        if i<=50:
-            continue
-        else:
-            print("It's last number")
+# Вывести N первых чисел кратные M и больше K
+N: int = int(input("Enter number: "))
+M: int = int(input("Enter number: "))
+K: int = int(input("Enter number: "))
 
-# while i % m == 0 and i > k:
-#    print(i)
-#    if counter != 6:
-#        continue
-#   break
+while N:
+    if K % M == 0:
+        print(K)
+        N -= 1
+    K += 1
