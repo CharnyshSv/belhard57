5af7b874838988ef4b93f45c9a62d28de0981a2f
CharnyshSv <sv.charnysh@gmail.com>
2022-07-12 23:51:08 +0300

    add resolve 1

--- 6_4.py
+++ 6_4.py
@@ -1,13 +1,16 @@
 # Дан список содержащий в себе различные типы данных, отфильтровать таким
 # образом, чтобы остались только строки, использование дополнительного списка
 # незаконно
-lst=[1, 10, 'pp', True, 'hello', 'a', 3, False, None]
+from typing import List
+
+lst: list[int | str | bool | None] = [1, 10, 'pp', True, 'hello', 'a', 3, False, None]
 
 def filter_terms(lst):
-    lst = []
+
     for i in lst:
         if isinstance(i, str):
             lst.append(i)
-            return lst
-
+        else:
+            lst.remove(i)
 print(filter_terms(lst))
+
