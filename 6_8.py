e9d4902e5ba460579706af93289e71eadf7e0186
CharnyshSv <sv.charnysh@gmail.com>
2022-07-17 22:39:23 +0300

    add resolve

--- 6_8.py
+++ 6_8.py
@@ -1,3 +1,13 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход
 # поступает город, необходимо сказать из какой он страны
 
+def find_country(city: str) -> str:
+    countries = {
+        "belarus": ["minsk", "gomel", "vitebsk", "brest", "grodno"],
+        "russian": ["msk", "spb"]
+    }
+    for country, cities in countries.items():
+        if city.lower() in cities:
+            return country.title()
+
+print(find_country("minsk"))
