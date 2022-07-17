9d3753da03be6f4a076b476680ae509881a771ce
CharnyshSv <sv.charnysh@gmail.com>
2022-07-17 21:09:06 +0300

    add resolve

--- 6_4.py
+++ 6_4.py
@@ -3,12 +3,26 @@
 # незаконно
 from typing import List
 
-lst = [1, 10, 'pp', True, 'hello', 'a', 3, False, None]
+list = [1, 10, 'pp', True, 'hello', 'a', 3, False, None]
 
 
-def filter_terms(lst):
-    lst = [i for i in lst if isinstance(i, str)]
+def filter_terms(lst: list) -> list:
+    list = [i for i in lst if isinstance(i, str)]
+    return list
+
+
+#print(filter_terms(list))
+
+# второй способ
+
+def str_sorted(lst: list) -> list:
+#    return lst = list(filter(lambda x: isinstance(x, str), lst))
+    i = 0
+    while i < len(lst):
+        if not isinstance(lst[i], str):
+            del lst[i]
+        else:
+            i += 1
     return lst
 
-
-print(filter_terms(lst))
+print(str_sorted([1, 10, 'pp', True, 'hello', 'a', 3, False, None, 'b']))
