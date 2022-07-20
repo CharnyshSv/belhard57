61bbb5c33e2a2c87f8a1275c72ceec30b7a80415
CharnyshSv <sv.charnysh@gmail.com>
2022-07-20 16:38:00 +0300

    add resolve

--- 8_1.py
+++ 8_1.py
@@ -1,3 +1,19 @@
 # Реализовать бинарный поиск элемента в отсортированном списке.
 # Дан список чисел, пользователь вводит число, необходимо реализовать поиск
 # введенного пользователем числа в этом списке с помощью бинарного поиска
+
+def binary_search(lst, num):
+    first = 0
+    last = len(lst) - 1
+    while first <= last:
+        midlle: int = (first + last) // 2
+        if lst[midlle] == num:
+            return lst[midlle]
+        elif lst[midlle] > num:
+            last = midlle - 1
+        else:
+            first = midlle + 1
+    return None
+
+my_lst = [1, 10, 20, 22, 30, 85, 88, 99, 101]
+print(binary_search(my_lst, 30))
