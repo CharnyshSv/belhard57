ee451af27e586a4215e2879cdd690461b2ca1ab8
CharnyshSv <sv.charnysh@gmail.com>
2022-07-29 20:19:57 +0300

    add input

---
+++ models/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from sqlalchemy import Column, SmallInteger, VARCHAR, TIMESTAMP, Boolean, Integer, ForeignKey, Text
+from sqlalchemy .orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__: str = "users"
+
+    id = Column(Integer, prymary_key=True)
+    user_name = Column(VARCHAR(24), unique=True, nullable=False)
+    hashed_password = Column(Text, nullable=False)
+    is_blocked = Column(Boolean, default=False)
+
+class Category(Base):
+    __tablename__: str = "categories"
+
+    id = Column(SmallInteger, prymary_key=True)
+    name = Column(VARCHAR(24), nullable=False)
+    parent_id = Column(SmallInteger, ForeignKey("categories.id", ondelete="CASCADE"))
+
+class Article(Base):
+    __tablename__: str = "articles"
+
+    id = Column(SmallInteger, prymary_key=True)
+    category_id = Column(
+        SmallInteger,
+        ForeignKey("categories.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    title = Column(VARCHAR(50), nullable=False)
+    body = Column(VARCHAR(1024), nullable=False)
+    date_created = Column(TIMESTAMP, default=datetime.utcnow())
+
+class UserArticle(Base):
+    __tablename__: str = "user_articles"
